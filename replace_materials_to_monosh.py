3f608e7dfc40a220f86ed6f69190ce5a1567dbb6
yanagi55 <hitoyanagi@gmail.com>
2023-04-27 14:48:28 +0900

    バックアップをとって置き換えられるようにした

--- replace_materials_to_monosh.py
+++ replace_materials_to_monosh.py
@@ -1,16 +1,26 @@
 import os
 from UnityEngine import Texture, Shader, Material
 from UnityEditor import AssetDatabase, Selection
+import shutil
 
 def create_material(material_name):
     # create a new material with the given name
     new_material = Material(Shader.Find("Silent/Filamented (Roughness setup)"))
     new_material.name = material_name
 
+
     # set the texture properties if they exist
-    albedo_texture_path = "Assets/SCANDINAVIAN_APMENT/Textures/T_wood_02_D.png"
-    normal_texture_path = "Assets/SCANDINAVIAN_APMENT/Textures/T_wood_02_N.png"
-    roughness_texture_path = "Assets/SCANDINAVIAN_APMENT/Textures/T_wood_02_R.png"
+
+    albedo_texture_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname((AssetDatabase.GetAssetPath(Selection.activeObject))))), "Textures")
+    albedo_texture_path = os.path.join(albedo_texture_dir, "T" + material_name[2:] + "_D.png")
+    
+    normal_texture_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname((AssetDatabase.GetAssetPath(Selection.activeObject))))), "Textures")
+    normal_texture_path = os.path.join(albedo_texture_dir, "T" + material_name[2:] + "_N.png")
+
+    roughness_texture_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname((AssetDatabase.GetAssetPath(Selection.activeObject))))), "Textures")
+    roughness_texture_path = os.path.join(albedo_texture_dir, "T" + material_name[2:] + "_R.png")
+
+
     if AssetDatabase.LoadAssetAtPath(albedo_texture_path, Texture):
         new_material.SetTexture(
             "_MainTex", AssetDatabase.LoadAssetAtPath(albedo_texture_path, Texture))
@@ -25,7 +35,8 @@ def create_material(material_name):
     new_material.SetInt("_Bakery", 3) # Note: Filamented Standardで、[None, SH, RMN, MonoSH]になってる
 
     # save the new material
-    asset_path = "Assets/SCANDINAVIAN_APMENT/Materials/INSTANCE/NewMaterials"
+    asset_path = (os.path.dirname((AssetDatabase.GetAssetPath(Selection.activeObject))))
+    print(asset_path)
     if not os.path.exists(asset_path):
         os.makedirs(asset_path)
     AssetDatabase.CreateAsset(
@@ -33,5 +44,19 @@ def create_material(material_name):
     AssetDatabase.SaveAssets()
 
 
-# create a new material using the texture path provided
-create_material("TestMaterial")
+# get the selected material objects
+selected_objects = Selection.objects
+for obj in selected_objects:
+    if obj.GetType() == Material:
+        material_name = obj.name
+
+        # create a backup of the original material
+        asset_path = AssetDatabase.GetAssetPath(obj)
+        backup_path = os.path.join(os.path.dirname(asset_path), "Backup")
+        if not os.path.exists(backup_path):
+            os.makedirs(backup_path)
+        shutil.copy(asset_path, os.path.join(backup_path, material_name + ".mat"))
+        print(backup_path)
+        
+        # create a new material using the selected material object's name
+        create_material(material_name)
